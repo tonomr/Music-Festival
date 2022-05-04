eeb611c0bd88379d9268517559528cbe76822eee
tonomr <tonomac7@gmail.com>
2022-05-04 02:55:25 -0500

    tickets, footer and smooth scroll added

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function startApp() {
   createGallery();
+  scrollNav();
+}
+
+function scrollNav() {
+  const links = document.querySelectorAll(".nav-main a");
+  links.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const sectionScroll = e.target.attributes.href.value;
+      const section = document.querySelector(sectionScroll);
+      section.scrollIntoView({ behavior: "smooth" });
+    });
+  });
 }
 
 function createGallery() {
